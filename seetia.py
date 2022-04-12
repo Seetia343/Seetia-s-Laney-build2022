08b0d09193718afd56559b30af10f59c8a17e9ce
Seetia343 <viagbo.seetia@gmail.com>
2022-04-12 00:03:09 -0700

    Seetia.py
    First programing

---
+++ seetia.py
@@ -0,0 +1,20 @@
+print("Hello world")
+
+name = "Seetia"
+age = 18
+years_at_hth = 2
+plays_baskteball = True
+jersey_number = "12"
+
+
+car_name = "honda"
+x = 50
+my_first_name = "john"
+
+print(name)
+print(age)
+print(years_at_hth)
+print(plays_baskteball)
+print(jersey_number)
+
+print(my_first_name)
