7f5836e9ad52258ca8f769486d2ac03bd94569a0
Seetia343 <viagbo.seetia@gmail.com>
2022-04-23 12:47:46 -0700

    Update operators.py

--- Data-analysis/operators.py
+++ Data-analysis/operators.py
@@ -42,6 +42,6 @@ numbers = [0,1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 for x in numbers:
     if x % 2 == 0:
-        print("even")
+        print(f"{x}even")
     else:
-        print("odd")
+        print(f"{x}odd")
