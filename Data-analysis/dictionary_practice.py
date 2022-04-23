d489f3e11823c7c2388de0f51f26e1d8d0d68798
Seetia343 <viagbo.seetia@gmail.com>
2022-04-23 12:45:54 -0700

    operators and iteration
    follow the instruction and fill in the blanks

---
+++ Data-analysis/dictionary_practice.py
@@ -0,0 +1,32 @@
+#Chicken = 1.59
+#Beef = 1.99
+#Cheese = 1.00
+#Milk =2.50
+
+#Step 2
+Food = {"Chicken" :1.59, "Beef":1.99, "Cheese":1.00, "Milk":2.50 } #food dictionary
+
+#Step 3
+Anime = {"One Piece":13 , "Bleach":4, "Naruto":10, "Jujestsu Kaisen":0} # animes and how much movies they have
+
+#Step 4
+One_piece_movies = Anime ["One Piece"] 
+
+#Step 5/storing each functions into their own variable
+Food_1 = Food["Chicken"]
+Food_2 = Food ["Beef"]
+Food_3 = Food ["Cheese"]
+Food_4 = Food ["Milk"]
+print(Food)
+
+#Step 6
+Bleach_movies = Anime["Bleach"]
+Naruto_movies = Anime["Naruto"]
+Jujestsu_movies = Anime["Jujestsu Kaisen"]
+
+#
+Anime ["Jujestsu Kaisen"] =1 
+
+
+
+
