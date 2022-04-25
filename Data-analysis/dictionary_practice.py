520d9fe55176e38eaae9b39ab30fbd21bf124c58
Seetia343 <viagbo.seetia@gmail.com>
2022-04-25 00:52:55 -0700

    Update dictionary_practice.py

--- Data-analysis/dictionary_practice.py
+++ Data-analysis/dictionary_practice.py
@@ -4,6 +4,9 @@
 #Milk =2.50
 
 #Step 2
+from turtle import Shape
+
+
 Food = {"Chicken" :1.59, "Beef":1.99, "Cheese":1.00, "Milk":2.50 } #food dictionary
 
 #Step 3
@@ -17,7 +20,7 @@ Food_1 = Food["Chicken"]
 Food_2 = Food ["Beef"]
 Food_3 = Food ["Cheese"]
 Food_4 = Food ["Milk"]
-print(Food)
+#print(Food_1)
 
 #Step 6
 Bleach_movies = Anime["Bleach"]
@@ -27,6 +30,20 @@ Jujestsu_movies = Anime["Jujestsu Kaisen"]
 #
 Anime ["Jujestsu Kaisen"] =1 
 
-
-
+Shoes ={"Joran_13":1, "yeezy":8, "Foamposite":10, "Air_max":5, "SB_dunk":20 }
+Shoes ["SB_dunk"] = 18
+Shoes ["yeezy"] = 9
+#Invotory increase
+Shoes ["Jordan_13"] = 8
+Shoes ["yeezy"] = 16
+Shoes["Foamposite"] = 17
+Shoes["Air_max"] = 12
+Shoes["SB_dunk"] =25 
+#print
+#Stepp VII
+Anime ["One piece"] =10
+#step VIII
+My_Shoes ={"Nike":6, "Foamposite":1, "Air_max":1, "SB_dunk":1 , "Vans":2 }
+My_Shoes ["Vans"] =1
+print(My_Shoes)
 
