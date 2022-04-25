557889e649420fb58e42fc7f88854a4ce52c65f9
Seetia343 <viagbo.seetia@gmail.com>
2022-04-25 01:14:52 -0700

    Create list_practice
    getting use to lists

---
+++ Data-analysis/list_practice.py
@@ -0,0 +1,18 @@
+CityList = ["Oakland", "Atlanta", "New York City","Seattle", "Memphis", "Miami", "Boston", "Los Angeles", "Denver", "New Orleans"]
+
+#three_cities= CityList[0:3]
+#print(three_cities)
+
+CityList[0] = "San Francisco"
+CityList[2] = "Brooklyn"
+CityList[7] = "Hollywood"
+CityList[5] = "Tampa"
+print(CityList)
+#US_ciites = CityList[4:8]
+#print(CityList)
+
+#RestaurantList = ["Bento&Bowls","burger king","T4","panda","chickfila","Evert Jone","Prima","Piedology","Boston Market","Taqueria"]
+#print(RestaurantList[1])
+
+#technologiesList =["Phone","Laptop","Tv","Game console"]
+#print(technologiesList)
