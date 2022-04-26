76758fff2c36e5e9d3456dd1c89c88217477f626
Seetia343 <viagbo.seetia@gmail.com>
2022-04-25 23:24:20 -0700

    Update list_practice.py

--- Data-analysis/list_practice.py
+++ Data-analysis/list_practice.py
@@ -1,15 +1,31 @@
-CityList = ["Oakland", "Atlanta", "New York City","Seattle", "Memphis", "Miami", "Boston", "Los Angeles", "Denver", "New Orleans"]
+US_cities = ["Oakland", "Atlanta", "New York City","Seattle", "Memphis", "Miami", "Boston", "Los Angeles", "Denver", "New Orleans"]
 
-#three_cities= CityList[0:3]
+#three_cities= US_cities[0:2]
 #print(three_cities)
 
-CityList[0] = "San Francisco"
-CityList[2] = "Brooklyn"
-CityList[7] = "Hollywood"
-CityList[5] = "Tampa"
-print(CityList)
-#US_ciites = CityList[4:8]
+US_cities[0] = "San Francisco"
+US_cities[2] = "Brooklyn"
+US_cities[7] = "Hollywood"
+US_cities[5] = "Tampa"
 #print(CityList)
+
+US_cities.append("New Jersey")
+US_cities.extend(["Santa Cruz", "Selma", "Chicago"])
+US_cities.insert(7, "Washington", "D.C.")
+
+
+del US_cities[3]
+US_cities.pop(6)
+US_cities.remove("New York City")
+#deleting 
+del US_cities[5]
+del US_cities[0]
+del US_cities[8]
+
+
+
+#US_ciites = CityList[4:8]
+#print(US_cities)
 
 #RestaurantList = ["Bento&Bowls","burger king","T4","panda","chickfila","Evert Jone","Prima","Piedology","Boston Market","Taqueria"]
 #print(RestaurantList[1])
