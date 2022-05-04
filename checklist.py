c6c7146e824d8fd144f5d03917a045c58fd4858d
Seetia343 <viagbo.seetia@gmail.com>
2022-05-03 20:12:33 -0700

    Update checklist.py

--- checklist.py
+++ checklist.py
@@ -6,41 +6,68 @@ You can then check or uncheck the entry's checkbox.
 '''
 
 # Create our Checklist
-checklist = 
+checklist = []
 
 # Define Functions
 def create(item):
     # Create item code here
+    checklist.append(f"[ ] {item}")
 
 def read(index):
     # Read code here
+    print( checklist[index] )
+    
 
 def update(index, item):
     # Update code here
+    checklist[index] = item 
 
 def destroy(index):
-    # Destroy code here
+    # Destroy code here -- you could also use list.pop(index)
+    del checklist[index]
 
 def mark_completed(index):
     # Add code here that marks an item as completed
+    if checklist[index][1] == " ":
+        checklist[index][1] = "√"
 
 def list_all_items():
     # List all items code here
+    for item in checklist:
+        print(item)
 
 def user_input(prompt):
     # Get user input here
+    input_item = input(prompt) 
+
+    return input_item
 
 def select(function_code):
     # User Selection Code here
-
     # Create item example
     if function_code == "C":
-        input_item = user_input("Input item:")
+        input_item = user_input("What would you like to add to the list: ")
         create(input_item)
+        running = True
 
-running = True
+        return True
 
-while running:
-    selection = user_input(
-        "Press C to add to list, R to Read from list, P to display list, and Q to quit")
-    running = select(selection)
+    elif function_code =="R" :
+        input_item = input("Enter the index position you are trying to access in the list:")
+    read(int(input_item))
+    running = True
+    
+    return running
+
+    elif function_code == "U" :
+        input_index = input("Enter the index position you are trying to update in the list: ")
+    input_item =input("Enter the element you are tryingto update to the list:")
+    update(int(input_index), input_item)
+    running = True
+
+    return running
+
+    while running:
+        selection = user_input(
+        "Press C to add to list, R to Read from list, P to display list, and Q to quit: ").upper()
+        running = select(selection)
